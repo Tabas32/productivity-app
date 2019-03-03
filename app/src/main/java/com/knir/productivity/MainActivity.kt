dead250fb4bfda90ce630b0317eb9e8d08b52537
Marian Sabat <sabat@localhost.company>
2019-03-03 22:39:48 +0100

    first buttons

--- app/src/main/java/com/knir/productivity/MainActivity.kt
+++ app/src/main/java/com/knir/productivity/MainActivity.kt
@@ -2,11 +2,25 @@ package com.knir.productivity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun startTimer(view : View){
+        tv_main_timer.text = "Started"
+    }
+
+    fun stopTimer(view : View){
+        tv_main_timer.text = "Stopped"
+    }
+
+    fun resetTimer(view : View){
+        tv_main_timer.text = "default"
     }
 }
